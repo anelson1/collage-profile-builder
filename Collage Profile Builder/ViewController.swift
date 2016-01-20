0c52eb23ab237ebb90da3a8bcf2db7ff6e62f3ef
Angelo <student@ravioli-ravioli-i-need-dank-memeiolis.local>
2016-01-20 11:36:12 -0600

    Initial Commit

---
+++ Collage Profile Builder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collage Profile Builder
+//
+//  Created by Student on 1/20/16.
+//  Copyright © 2016 Dank Memes and Son International Shipping Company Express. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
